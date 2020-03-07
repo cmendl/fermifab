c8bee87e74a66e4c1767815333a397b0754139c9
Christian B. Mendl <christian.b.mendl@gmail.com>
2020-03-07 02:43:14 +0100

    integrating 'tensor_op' into Python module (currently only real-valued operators supported)

--- python/fermifab/__init__.py
+++ python/fermifab/__init__.py
@@ -9,3 +9,4 @@ FermiFab toolbox for fermionic many-particle quantum systems.
 from .fermistate import *
 from .fermiop    import *
 from .rdm        import *
+from .tensor_op  import *
---
+++ python/fermifab/tensor_op.py
@@ -0,0 +1,24 @@
+import fermifab.kernel
+from scipy.sparse import csr_matrix
+from fermifab.fermiop import FermiOp
+
+__all__ = ['tensor_op']
+
+
+def tensor_op(op, N):
+    """
+    Calculate the matrix representation of the N-fold tensor product of an operator.
+
+    Args:
+        op: quantum operator of type 'FermiOp', with 'pFrom' and 'pTo' equal to 1
+        N:  number of tensor factors
+
+    Returns:
+        FermiOp: N-fold tensor product
+    """
+    if op.pFrom != 1 or op.pTo != 1:
+        raise ValueError('operator particle numbers must be equal to 1')
+    dims, val, ind = fermifab.kernel.tensor_op(op.data, N)
+    # finally convert to dense matrix, for simplicity
+    AN = csr_matrix((val, (ind[:, 0], ind[:, 1])), shape=dims).todense()
+    return FermiOp(op.orbs, N, N, data=AN)
