5c3f35c9f1d2bf044abe19141f880627560e3bb2
Christian B. Mendl <christian.b.mendl@gmail.com>
2020-04-15 03:55:09 +0200

    trying sudo with apt-get

--- python/doc/setup.py
+++ python/doc/setup.py
@@ -1,4 +1,4 @@
 import subprocess
 
 # workaround to install the required LAPACKE library during "Read the Docs" build process
-subprocess.run('apt install libblas-dev liblapacke-dev', shell=True, check=True)
+subprocess.run('sudo apt-get install libblas-dev liblapacke-dev', shell=True, check=True)
