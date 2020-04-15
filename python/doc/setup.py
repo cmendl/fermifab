fee7c0907d9851ad4380a5f42f33df2472ffbc5b
Christian B. Mendl <christian.b.mendl@gmail.com>
2020-04-15 04:36:33 +0200

    next attempt to fix the Read the Docs build process

--- python/doc/setup.py
+++ python/doc/setup.py
@@ -1,4 +1,5 @@
 import subprocess
 
 # workaround to install the required LAPACKE library during "Read the Docs" build process
-subprocess.run('sudo apt-get install libblas-dev liblapacke-dev', shell=True, check=True)
+subprocess.run('apt-get -y install sudo', shell=True, check=True)
+subprocess.run('sudo apt-get -y install libblas-dev liblapacke-dev', shell=True, check=True)
